289f3fd1c91111fcb9b81eab41752e9f091c6ff8
塚原 <tsukahara.medicalit@gmail.com>
2023-11-10 15:01:06 +0900

    コンソールの追加

---
+++ Black Jack team/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Black_Jack_team
+{
+    internal class test
+    {
+        static void Main()
+        {
+            Console.Write("gomi");
+            Console.Write("森祐樹");
+            ///
+        }
+        
+    }
+}
