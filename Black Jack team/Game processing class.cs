d71129239af6567aa4bd525b5d11eec0456b29f4
本 <ooo12mk@icloud.com>
2024-01-10 13:05:45 +0900

    前回休んでしまいすみませんでした。

--- Black Jack team/Game processing class.cs
+++ Black Jack team/Game processing class.cs
@@ -10,20 +10,32 @@ namespace Black_Jack_team
     {
         public Chip_Variation_Class chip = new Chip_Variation_Class();
         public Deck_card_class Card = new Deck_card_class();
-        int my;
-        public string myhiitano="kuroba6c4";
-        public string deelhiitano = "c5c4d13";
+        int mydata, deeldata;
+        public string mydraw = "";
+        public string deeldraw = "";
         public  void BetDecision()
         {
 
         }
+        public void Goukeiti()
+        {
 
+        } 
         public void DoubleDecision(int bet, int my, int deel)
         {
             //カードを引く
+            mydraw = Card.IllGiveYouOneInMyHand();
+
             //合計値を何とかするメソッド
+            mydata = henkan(mydraw);
+
             //ディーラーがカードを引いたりする
+            deeldraw = Card.IllGiveYouOneInMyHand();
+            //ディーラー17になるまでループ？
+
             //合計値を何とかするメソッド
+            deeldata = henkan(deeldraw);
+
             if (Judge(my, deel))
             {
                 chip.IncreaseInPossessions(bet * 2 * 2);
@@ -35,9 +47,11 @@ namespace Black_Jack_team
 
         }
 
-        public void Cardhiki()
+        public void henkan(int data,string dada)
         {
-
+            string[] split = dada.Split('_');
+            data= int.Parse(split[1]);
+            
             return;
         }
 
